b1978fe295abe098bd99a1434b5a360b1cbd7777
Madis Vellamäe <madis.vellamae@cloudmore.com>
2025-09-09 13:16:15 +0000

    Implement task creation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import crypto from "crypto";
 
 type Task = {
   id: string;
@@ -16,12 +17,27 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.get("/tasks", (req, res) => {
   res.json(Array.from(taskMap.values()));
+});
+
+app.post("/tasks", (req, res) => {
+  const id = crypto.randomUUID();
+
+  const title = String(req.body?.title ?? "").trim();
+
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  const newTask: Task = { id: id, done: false, title: title };
+  taskMap.set(id, newTask);
+
+  res.status(201).json(newTask);
 });
 
 app.listen(port, () => {
